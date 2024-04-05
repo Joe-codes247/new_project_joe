91e548c6dde78bfa2025f7a1882aa0be40f02f35
myname <myemail>
2024-04-05 08:11:01 +0300

    Learning web development in Django

---
+++ registration/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('registration/', views.registration, name='registration'),
+    path("registerpage/", views.mypage, name='myregisterpage') #views.mypage calls the function in views.py line
+]

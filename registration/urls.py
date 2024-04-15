db5582bae867d719c94ce177a1b2961f2f64a52c
myname <myemail>
2024-04-15 10:56:46 +0300

    Creating a functional Form

--- registration/urls.py
+++ registration/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('registration/', views.registration, name='registration'),
-    path("registerpage/", views.mypage, name='myregisterpage') #views.mypage calls the function in views.py line
+    path("registerpage/", views.mypage, name='myregisterpage'), #views.mypage calls the function in views.py line
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('addstudent', views.addstudent, name='addingstudent'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -8,4 +8,25 @@ def mypage(request):
     template = loader.get_template("register.html")
     return HttpResponse(template.render())
 
+def home(request):
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render())
+
+def about(request):
+    template = loader.get_template("About us.html")
+    return HttpResponse(template.render())
+
+def contacts(request):
+    template = loader.get_template("contacts.html")
+    return HttpResponse(template.render())
+
+def addstudent(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        query = Student(email=email, password=password)
+        query.save()
+
+
+
 
